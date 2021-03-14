377c764118ebff5541e51ed88ad20f37e2397a67
Joseph Jerry <josephjeremiah564@yahoo.com>
2021-03-14 06:35:36 +0100

    minor change

--- routes/MessageRoute.js
+++ routes/MessageRoute.js
@@ -3,6 +3,6 @@ const messageRouter = express.Router();
 const MessageCtrl = require("../controllers/MessageCtrl");
 
 messageRouter.get("/", MessageCtrl.getHome);
-messageRouter.post("/messages", MessageCtrl.postMessage);
+messageRouter.post("/", MessageCtrl.postMessage);
 
 module.exports = messageRouter;
